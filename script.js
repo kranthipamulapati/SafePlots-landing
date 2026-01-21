778bde3d11512f1efe5481bb81b88398dc52c5cb
Pamulapati Kranthi Kumar Reddy <pamulapati.kranthi@gmail.com>
2026-01-21 06:48:50 +0530

    header toggle in mobile

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 /** @format */
 
 console.log("initialized.");
+
+const header = document.querySelector(".header");
+const mobileToggle = document.querySelector(".mobile-toggle");
+
+if (mobileToggle) {
+    mobileToggle.addEventListener("click", () => {
+        header.classList.toggle("active");
+
+        // Toggle Icon
+        if (header.classList.contains("active")) {
+            mobileToggle.textContent = "✕"; // Close icon
+        } else {
+            mobileToggle.textContent = "☰"; // Menu icon
+        }
+    });
+}
